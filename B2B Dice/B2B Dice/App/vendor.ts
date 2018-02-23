4d5cac1339df770132d178fe6e49e25f9775a64e
ChristopherAllen777 <chris_allen1@dellteam.com>
2018-02-23 10:56:57 -0600

    add vendor.ts file

---
+++ B2B Dice/B2B Dice/App/vendor.ts
@@ -0,0 +1,24 @@
+﻿// Angular
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+import '@angular/core';
+import '@angular/common';
+import '@angular/forms';
+import '@angular/http';
+import '@angular/router';
+import 'angular-in-memory-web-api';
+
+// RxJS
+import 'rxjs';
+
+// Other vendors for example JQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
+require("../styles.css");
+require("../External_Scripts/JS/jquery.min.js");
+require("../node_modules/primeng/resources/themes/omega/theme.css");
+require("../node_modules/primeng/resources/primeng.min.css");
+require("../Assets/favicon.ico");
+require("../node_modules/dragula/dist/dragula.min.css");
+require("../External_Scripts/CSS/bootstrap/css/bootstrap.min.css");
+require("../External_Scripts/JS/bootstrap.min.js");
+require("../External_Scripts/CSS/font-awesome/css/font-awesome.min.css");
