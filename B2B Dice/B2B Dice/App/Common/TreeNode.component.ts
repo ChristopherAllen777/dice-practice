8a09753be592fec408718b46e7488006dd91279e
ChristopherAllen777 <chris_allen1@dellteam.com>
2018-02-23 15:40:32 -0600

    Adding TreeNode.component.html

---
+++ B2B Dice/B2B Dice/App/Common/TreeNode.component.ts
@@ -0,0 +1,34 @@
+﻿import { Component, Input, OnInit } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { FormsModule } from "@angular/forms";
+
+@Component({
+    selector: 'tree-node',
+    templateUrl: './TreeNode.component.html'
+})
+export class TreeNodeComponent implements OnInit {
+    public arrayOfKeys: string[] = [];
+    public childArrays: string[] = [];
+    public childObjects: string[] = [];
+
+    @Input() node: any;
+
+    ngOnInit() {
+        let keys = Object.keys(this.node);
+        for (let i = 0; i < keys.length; i++) {
+            let key = keys[i];
+            if (this.node[key] != null) {
+                this.arrayOfKeys.push(key);
+            }
+        }
+        for (let key of this.arrayOfKeys) {
+            if (typeof this.node[key] == 'object' && this.node[key] != null) {
+                if (this.node[key].constructor.name == 'Object') {
+                    this.childObjects.push(key);
+                } else if (this.node[key].constructor.name == 'Array') {
+                    this.childArrays.push(key)
+                }
+            }
+        }
+    }
+}
